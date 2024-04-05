e95e3c75bcddaa176bd3bb81f9cfc265121b6ab3
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-05 02:35:19 -0700

    HTM_CSS_PROJECT

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,11 @@
+const product = {
+    title: "ball pen",
+    ratting :4,
+    offer:  5,
+    price: 270,
+};
+
+
+product ["tittle"]= "mandeep kaur";
+
+console.log(product["title"]);
---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,36 @@
+
+// print 1 to 5
+//for(let count = 1; count <= 5; count ++){
+  //  console.log ("mandeep kaur);
+
+
+
+
+// while loop 
+
+//let i = 1 ;
+//while (i <= 5){
+  //console.log ("mandeep kaur ");
+  //i ++; 
+//}
+
+
+//do - while-loop 
+
+//let i = 1;
+//do {
+  //console.log ("i = ", i) 
+  //i ++ ;
+//} while (i <= 10 ); 
+
+
+
+// for- of- loop  
+
+//let str = "mandeep kaur ";
+//let size = 0;
+//for (let i of str ){
+  console.log ("i = ", i );
+  size++ ;
+}
+console.log ("string size = ", size ); 
