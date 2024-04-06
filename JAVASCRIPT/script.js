69b3bf9d5a5eba7d1542b9480a0da72f432f5451
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-06 06:03:21 -0700

    HTM_css_project

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -30,7 +30,46 @@
 //let str = "mandeep kaur ";
 //let size = 0;
 //for (let i of str ){
-  console.log ("i = ", i );
-  size++ ;
+ // console.log ("i = ", i );
+  //size++ ;
+//}
+//console.log ("string size = ", size ); 
+
+
+
+//for-in-loop
+
+
+//let student ={
+  //name: "mandeep kaur",
+  //age: 20,
+  //cgpu: 7.5,
+  //ispass: true,
+//};
+
+
+//for (let key  in student){
+  //console.log ("key = ", key , " value =", student [key]);
+//}
+
+
+
+// practice Qs1
+
+//for (let num = 0;  num <= 100 ; num ++){
+  //if (num % 2 == 0){
+    //console.log ("num = ", num);
+  //}
+//}
+
+
+//practice Qs2 
+
+let gameNum = 25;
+let userNum = prompt ("guess the game number;");
+
+while (userNum !== gameNum ){
+  userNum = prompt ("your entered wrong number . guess agin:");
+
 }
-console.log ("string size = ", size ); 
+console.log ("congratulations, you entered right number"); 
