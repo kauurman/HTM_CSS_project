209f456c34be065616b57850d954bc694503a172
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-08 04:54:17 -0700

    htm_css_project

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,15 +1,21 @@
-let obj = {
-
-  item: "pen ",
-  price: 10,
-};
-
-console.log ("the cost of " , obj, itm ,"is", obj.price, "rupess");
+//let heroes = ["thor","spideman", "ironman", " shaktiman", "hulk"];
+// for loop 
+//for (let idx= 0; idx <heroes.length; idx++){
+  //  console.log (heroes[idx]);
+//}
 
 
+// for of 
 
-// template literals 
+//for (let hero of heroes){
+  //  console.log (hero);
+//}
 
-let specialstring = `this is a template literal`
-console. log (specialstring );
+
+
+let  cities = ["delhi","punjab","hyderabad","pune","mumbai","goa",""];
+for (let city of cities ){
+    console.log (city. toUpperCase());
+}
+
 
