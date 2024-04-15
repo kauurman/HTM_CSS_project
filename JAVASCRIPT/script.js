06d970a1026f80f411163958f68fca1dae9887c3
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-15 03:19:47 -0700

    javascript

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,6 +1,3 @@
 let marvelHeroes = ["thor","spidemam","iromam"];
-let dcHeroes = ["supermam","batman"];
-let indianHeroes = ["shaktiman","krish"]
-
-let heroes = marvelHeroes .concat(dcHeroes,indianHeroes );
-console.log (heroes);
+let val = marvelHeroes.shift();
+console.log ("deleted ",val);
