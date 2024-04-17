28274754013c0d579a2988c7d8a6b2dea0044e34
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-17 02:16:30 -0700

    htm_css_project

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,3 +1,11 @@
-let marvelHeroes = ["thor","spidemam","iromam"];
-let val = marvelHeroes.shift();
-console.log ("deleted ",val);
+let arr = [1,2,3,4,5,6,7];
+//arr.splice (2,2,101,102);
+
+//Add element 
+//arr.splice (2,0,101);
+
+//delete element 
+//arr.splice (3,1);
+
+//replace element 
+arr.splice(3,1,101);
