a12795fba733ff039b84957d2a52f136f604661e
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-27 06:00:23 -0700

    htm_css_project

---
+++ abdul-kalam.png/JAVASCPIRT/script.js
@@ -0,0 +1,28 @@
+let btn1 = document.querySelector("#btn1");
+btn1. onclick =() => {
+    console.log ("HANDLER1");
+    let a = 25;
+    a ++;
+    console.log (a); // 26
+};
+
+
+
+btn1. onclick =() => {
+    console.log ("HANDER2");
+};    
+
+
+
+
+
+
+
+let div = document.querySelector ("div");
+div.onmouscover = () => {
+
+    console.log ("you are inside div");
+};
+
+
+
