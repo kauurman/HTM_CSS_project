f586eb23349977aee0ab9c731cba5a766a6f88cb
karanpreet-singh-iic <karanpreet.singh@iic.ac.in>
2024-04-27 04:13:20 -0700

    htm_css_project

--- abdul-kalam.png/JAVASCPIRT/script.js
+++
@@ -1,9 +0,0 @@
-//let heading = document.getElementById("heading");// h1
-//console.log (heading);
-
-
-
-
-let headings = document.getElementsByClassName("heading-class");
-console.dir(headings);
-console.dir(headings);
